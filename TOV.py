fa20e22098c8a92c2ea3447fd30298bac2fbc354
denis <denis.arruga@gmail.com>
2020-09-08 21:28:00 +0200

    updated code

--- TOV.py
+++ TOV.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg as npla
 from scipy.integrate import odeint as solv
+from scipy.integrate import cumtrapz as integcum
+from scipy.integrate import trapz as integ
 
 c2 = cst.c**2
 kappa = 8*np.pi*cst.G/c2**2
 k = 1.475*10**(-3)*(cst.fermi**3/(cst.eV*10**6))**(2/3)*c2**(5/3)
+massSun = 1.989*10**30
 
 #Equation of state
 def PEQS(rho):
@@ -74,7 +77,7 @@ def f2(r, P, m, Psi, Phi, option):
     return A+B
 
 #Equation for dPsi/dr
-def f4(r, P, m, Psi, Phi, option):
+def f4(r, P, m, Psi, Phi, option, dilaton_active):
     ADOTA = adota(r, P, m, Psi, Phi)
     BDOTB = bdotb(r, P, m, Psi, Phi, option)
     rho = RhoEQS(P)
@@ -82,83 +85,133 @@ def f4(r, P, m, Psi, Phi, option):
     T = -c2*rho + 3*P
     A = (-Psi/2)*(ADOTA-BDOTB+4/r)
     B = b(r,m)*kappa*Phi**(1/2)*(T-Lm)/3
-    return A+B
+    if dilaton_active:
+        return A+B
+    else:
+        return 0
 
 #Equation for dPhi/dr
-def f3(r, P, m, Psi, Phi, option):
-    return Psi
+def f3(r, P, m, Psi, Phi, option, dilaton_active):
+    if dilaton_active:
+        return Psi
+    else:
+        return 0
 
 #Define for dy/dr
-def dy_dr(y, r, option):
+def dy_dr(y, r, option, dilaton_active):
     P, M, Phi, Psi = y
-    dy_dt = [f1(r, P, M, Psi, Phi, option), f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option),f4(r, P, M, Psi, Phi, option) ]
+    dy_dt = [f1(r, P, M, Psi, Phi, option), f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option, dilaton_active),f4(r, P, M, Psi, Phi, option, dilaton_active) ]
     return dy_dt
 
 #Define for dy/dr out of the star
-def dy_dr_out(y, r, P, option):
+def dy_dr_out(y, r, P, option, dilaton_active):
     M, Phi, Psi = y
-    dy_dt = [f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option),f4(r, P, M, Psi, Phi, option) ]
+    dy_dt = [f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option, dilaton_active),f4(r, P, M, Psi, Phi, option, dilaton_active) ]
     return dy_dt
 
 class TOV():
 
-    def __init__(self, initDensity, initPsi, initPhi, radiusMax, Npoint, option):
+    def __init__(self, initDensity, initPsi, initPhi, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active, log_active):
 #Init value
         self.initDensity = initDensity
         self.initPressure = PEQS(initDensity)
         self.initPsi = initPsi
         self.initPhi = initPhi
-        self.initMass = 0.000000000000001
+        self.initMass = 0
         self.option = option
+        self.dilaton_active = dilaton_active
+        self.log_active = log_active
+
 #Computation variable
-        self.radiusMax = radiusMax
+        self.radiusMax_in = radiusMax_in
+        self.radiusMax_out = radiusMax_out
         self.Npoint = Npoint
 #Star data
         self.Nstar = 0
         self.massStar = 0
         self.pressureStar = 0
         self.radiusStar = 0
-#Output vector
+#Output data
         self.pressure = 0
         self.mass = 0
         self.Phi = 0
         self.Psi = 0
         self.radius = 0
-        self.metric11 = 0
-        self.metric00 = 0
+        self.g_tt = 0
+        self.g_rr = 0
+        self.phi_inf = 0
 
     def Compute(self):
+        if self.log_active == True:
+            print('===========================================================')
+            print('SOLVER INSIDE THE STAR')
+            print('===========================================================\n')
+            print('Initial density: ', self.initDensity, ' MeV/fm^3')
+            print('Initial pressure: ', self.initPressure/10**12, ' GPa')
+            print('Initial mass: ', self.initMass/massSun, ' solar mass')
+            print('Initial phi: ', self.initPhi)
+            print('Initial psi: ', self.initPsi)
+            print('Number of points: ', self.Npoint)
+            print('Radius max: ', self.radiusMax_in/1000, ' km')
         y0 = [self.initPressure,self.initMass,self.initPhi,self.initPsi]
-        r = np.linspace(0.01,self.radiusMax,self.Npoint)
-
-        # Inside the star----------------------------------------------------------------------------
-        sol = solv(dy_dr, y0, r, args=(self.option,))
-        i = 0
-        while(sol[i,0]-sol[i+1,0]>0 and sol[i,0]>10**10):
-            i = i+1
-        self.pressure = sol[0:i:1,0]
-        self.Nstar = np.size(self.pressure)
-        self.mass = sol[0:i:1,1]
-        self.Phi = sol[0:i:1,2]
-        self.Psi = sol[0:i:1,3]
-        self.radius = r[0:i:1]
-        # Value at the radius of star----------------------------------------------------------------
+        if self.log_active == True:
+            print('y0 = ', y0,'\n')
+        r = np.linspace(0.01,self.radiusMax_in,self.Npoint)
+        if self.log_active == True:
+            print('radius min ',0.01)
+            print('radius max ',self.radiusMax_in)
+        sol = solv(dy_dr, y0, r, args=(self.option,self.dilaton_active))
+        # condition for Pressure = 0
+        self.Nstar = len(sol[sol[:,0]>10,0])-1
+        self.pressure = sol[0:self.Nstar,0]
+        self.mass = sol[0:self.Nstar:1,1]
+        self.Phi = sol[0:self.Nstar:1,2]
+        self.Psi = sol[0:self.Nstar:1,3]
+        self.radius = r[0:self.Nstar:1]
+        # Value at the radius of star
         self.massStar = self.mass[-1]
         self.radiusStar = self.radius[-1]
         self.pressureStar = self.pressure[-1]
-        # Outside the star---------------------------------------------------------------------------
+        if self.log_active == True:
+            print('Star radius: ', self.radiusStar/1000, ' km')
+            print('Star Mass: ', self.massStar/massSun, ' solar mass')
+            print('Star pressure: ', self.pressureStar, ' Pa\n')
+        if self.log_active == True:
+            print('===========================================================')
+            print('SOLVER OUTSIDE THE STAR')
+            print('===========================================================\n')
         y0 = [self.massStar, self.Phi[-1],self.Psi[-1]]
-        r = np.linspace(self.radiusStar,self.radiusMax,self.Npoint)
-        sol = solv(dy_dr_out, y0, r, args=(0,self.option))
-        self.pressure = np.concatenate([self.pressure, np.zeros(self.Npoint)])
-        self.mass = np.concatenate([self.mass, sol[:,0]])
-        self.Phi = np.concatenate([self.Phi, sol[:,1]])
-        self.Psi = np.concatenate([self.Psi,  sol[:,2]])
-        self.radius = np.concatenate([self.radius, r])
-        self.metric11 = b(self.radius, self.mass)
-        #self.metric00 =
+        if self.log_active == True:
+            print('y0 = ', y0,'\n')
+        r = np.linspace(self.radiusStar,self.radiusMax_out,self.Npoint)
+        if self.log_active == True:
+            print('radius min ',self.radiusStar)
+            print('radius max ',self.radiusMax_out)
+        sol = solv(dy_dr_out, y0, r, args=(0,self.option,self.dilaton_active))
+        self.pressure = np.concatenate([self.pressure, np.zeros(self.Npoint-1)])
+        self.mass = np.concatenate([self.mass, sol[1:,0]])
+        self.Phi = np.concatenate([self.Phi, sol[1:,1]])
+        self.Psi = np.concatenate([self.Psi,  sol[1:,2]])
+        self.radius = np.concatenate([self.radius, r[1:]])
+        self.phi_inf = self.Phi[-1]
+        if self.log_active == True:
+            print('Phi at infinity ', self.phi_inf)
+        # Compute metrics
+        self.g_rr = b(self.radius, self.mass)
+        a_dot_a = adota(self.radius, self.pressure, self.mass, self.Psi, self.Phi)
+        self.g_tt = np.exp(np.concatenate([[0.0], integcum(a_dot_a,self.radius)])-integ(a_dot_a,self.radius))
+        if self.log_active == True:
+            print('===========================================================')
+            print('END')
+            print('===========================================================\n')
 
-    def PlotEvolution(self):
+
+    def ComputeTOV(self):
+        self.Compute()
+        self.initPhi = self.initPhi/self.phi_inf
+        self.Compute()
+
+    def Plot(self):
         plt.subplot(221)
         plt.plot([x/10**3 for x in self.radius], [x for x in self.pressure])
         plt.xlabel('Radius r (km)')
@@ -166,7 +219,7 @@ class TOV():
         plt.axvline(x=self.radiusStar/10**3, color='r')
 
         plt.subplot(222)
-        plt.plot([x/10**3 for x in self.radius], [x/(1.989*10**30) for x in self.mass])
+        plt.plot([x/10**3 for x in self.radius], [x/massSun for x in self.mass])
         plt.xlabel('Radius r (km)')
         plt.title('Mass $M/M_{\odot}$', fontsize=12)
         plt.axvline(x=self.radiusStar/10**3, color='r')
@@ -185,13 +238,17 @@ class TOV():
 
         plt.show()
 
+    def PlotMetric(self):
+        plt.subplot(121)
+        plt.plot([x/10**3 for x in self.radius], self.g_tt)
+        plt.xlabel('Radius r (km)')
+        plt.title('g_tt', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
+        plt.subplot(122)
+        plt.plot([x/10**3 for x in self.radius], self.g_rr)
+        plt.xlabel('Radius r (km)')
+        plt.title('g_rr', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
-    def Phi_infini(self):
-        return self.Phi[-1]
-
-    def Phi_r(self):
-        M = np.zeros(len(self.Phi))
-        for i in range(len(self.Phi)):
-            M[i] = self.Phi[i]-self.initPhi
-        plt.plot([x/10**3 for x in self.radius[:]], M)
+        plt.show()
--- main.py
+++ main.py
@@ -3,145 +3,181 @@ from TOV import *
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy.constants as cst
+import numpy as np
 
 def test():
-    rhoInit = 10*cst.eV*10**6/(cst.c**2*cst.fermi**3)
-    radiusMax = 10000000
-    Npoint = 100000
+    PhiInit = 1
     PsiInit = 0
-    PhiInit = 2/2.0005115957706754/1.0000000175430597
     option = 1
-    dilaton = True
-    tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax, Npoint, option)
-    tov.Compute()
-    tov.PlotEvolution()
-    print(tov.Phi_infini())
-    print(tov.radiusStar)
+    radiusMax_in = 50000
+    radiusMax_out = 1000000
+    Npoint = 50000
+    log_active = False
+    dilaton_active = True
+    rhoInit = 100*cst.eV*10**6/(cst.c**2*cst.fermi**3)
+    tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active, log_active)
+    tov.ComputeTOV()
+    tov.Plot()
+    tov.PlotMetric()
 
+def findSameMass(mass):
+    """
+    input:
+    - mass: mass in solar mass
+    output:
+    - radius
+    """
+    PhiInit = 1
+    PsiInit = 0
+    option = 1
+    radiusMax_in = 50000
+    radiusMax_out = 10000000
+    Npoint = 50000
+    rhoMin = 100
+    rhoMax = 8200
+    log_active = False
+    rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in range(rhoMin,rhoMax,1)]
+    massStar_GR = np.array([])
+    massStar_ER = np.array([])
+    radiusStar_GR = np.array([])
+    radiusStar_ER = np.array([])
+    for iRho in rho:
+        rhoInit = iRho
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, False, log_active)
+        tov.ComputeTOV()
+        massStar_GR = np.append(massStar_GR,tov.massStar)
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, True, log_active)
+        tov.ComputeTOV()
+        massStar_ER = np.append(massStar_ER,tov.massStar)
+    rho = np.array(rho)/(cst.eV*10**6/(cst.c**2*cst.fermi**3))
+    # ER
+    massStar_ER = massStar_ER/(1.989*10**30)-mass
+    bool_sup_zero_ER = (massStar_ER>0)
+    ind = np.where((bool_sup_zero_ER[1:-1] == bool_sup_zero_ER[0:-2]) == False)
+    rho_ER = rho[ind]
+    sol_acc_ER = 100*massStar_ER[ind]/mass
+    if len(rho_ER)==0:
+        print('no solution')
+    else:
+        print('density in [MeV/fm^3]: ', rho_ER)
+        print('mass accuracy un %: ', sol_acc_ER)
+    # GR
+    massStar_GR = massStar_GR/(1.989*10**30)-mass
+    bool_sup_zero_GR = (massStar_GR>0)
+    ind = np.where((bool_sup_zero_GR[1:-1] == bool_sup_zero_GR[0:-2]) == False)
+    rho_GR = rho[ind]
+    sol_acc_GR = 100*massStar_GR[ind]/mass
+    if len(rho_GR)==0:
+        print('no solution')
+    else:
+        print('density in [MeV/fm^3]: ', rho_GR)
+        print('mass accuracy un %: ', sol_acc_GR)
 
-def plotDensityMass(PsiInit, PhiInit, option, dilaton):
-    radiusMax = 50000
-    Npoint = 1000
-    rhoMin = 10
-    rhoMax = 5000
+    plt.plot(rho, massStar_ER+mass)
+    for i in range(len(rho_ER)):
+        plt.axvline(rho_ER[i])
+    plt.show()
 
-    massStar = []
+def plotRelation():
+    PhiInit = 1
+    PsiInit = 0
+    option = 1
+    radiusMax_in = 50000
+    radiusMax_out = 10000000
+    Npoint = 50000
+    rhoMin = 100
+    rhoMax = 8200
+    log_active = False
+    massStar_GR = []
+    massStar_ER = []
+    radiusStar_GR = []
+    radiusStar_ER = []
     rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in range(rhoMin,rhoMax,100)]
     for iRho in rho:
         rhoInit = iRho
-        tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax, Npoint, option)
-        tov.Compute()
-        massStar.append(tov.massStar)
-    if dilaton:
-        plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/(1.989*10**30) for x in massStar], label='$\Phi_0=$%.1f' %PhiInit)
-    else:
-        plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/(1.989*10**30) for x in massStar], label='GR')
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, False, log_active)
+        tov.ComputeTOV()
+        massStar_GR.append(tov.massStar)
+        radiusStar_GR.append(tov.radiusStar)
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, True, log_active)
+        tov.ComputeTOV()
+        massStar_ER.append(tov.massStar)
+        radiusStar_ER.append(tov.radiusStar)
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/(1.989*10**30) for x in massStar_GR], label='GR')
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/(1.989*10**30) for x in massStar_ER], label='ER')
     plt.xlabel('Density $\\rho$ ($Mev/fm^3$)')
     plt.ylabel('Mass $M/M_{\odot}$')
     plt.legend()
-
-def plotRadiusMass(PsiInit, PhiInit, option, dilaton):
-    radiusMax = 100000
-    Npoint = 1000
-    rhoMin = 10
-    rhoMax = 100000
-
-    massStar = []
-    radiusStar = []
-    rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in range(rhoMin,rhoMax,100)]
-    for iRho in rho:
-        rhoInit = iRho
-        tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax, Npoint, option)
-        tov.Compute()
-        massStar.append(tov.massStar)
-        radiusStar.append(tov.radiusStar)
-    if dilaton:
-        plt.plot([x/1000 for x in radiusStar], [x/(1.989*10**30) for x in massStar], label='$\Phi_0=$%.1f' %PhiInit)
-    else:
-        plt.plot([x/1000 for x in radiusStar], [x/(1.989*10**30) for x in massStar], label='GR')
-    plt.xlabel('Radius R (km)')
+    plt.show()
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/1000 for x in radiusStar_GR], label='GR')
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/1000 for x in radiusStar_ER], label='ER')
+    plt.xlabel('Density $\\rho$ ($Mev/fm^3$)')
+    plt.ylabel('Radius (km)')
+    plt.legend()
+    plt.show()
+    plt.plot([x/1000 for x in radiusStar_GR], [x/(1.989*10**30) for x in massStar_GR], label='GR')
+    plt.plot([x/1000 for x in radiusStar_ER], [x/(1.989*10**30) for x in massStar_ER], label='ER')
+    plt.xlabel('Radius (km)')
     plt.ylabel('Mass $M/M_{\odot}$')
     plt.legend()
+    plt.show()
 
-def plotMass():
-    radiusMax = 100000
-    Npoint = 1000
-    rhoMin = 10
-    rhoMax = 1000
+def plotFig4():
+    PhiInit = 1
     PsiInit = 0
     option = 1
-
-    Phi = linspace(0.5,6,20)
-    rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in linspace(rhoMin,rhoMax,20)]
-    massStar = zeros((len(Phi),len(rho)))
-    for i in range(len(Phi)):
-        print(Phi[i])
-        for j in range(len(rho)):
-            PhiInit = Phi[i]
-            rhoInit = rho[j]
-            tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax, Npoint, option)
-            tov.Compute()
-            massStar[i][j] = tov.massStar
-    figure()
-    imshow(massStar/(1.989*10**30),extent=[min(rho)/(cst.eV*10**6/(cst.c**2*cst.fermi**3)), max(rho)/(cst.eV*10**6/(cst.c**2*cst.fermi**3)), max(Phi), min(Phi)],aspect='auto', cmap='inferno', origin='upper')
-    title("Mass $M/M_{\odot}$")
-    ylabel("Dilaton $\Phi_0$")
-    xlabel("Density $\\rho$ ($Mev/fm^3$)")
-    colorbar()
-
-def plotPhi_infini():
-    radiusStep = 100
+    radiusMax_in = 50000
+    radiusMax_out = 10000000
+    Npoint = 50000
     rhoMin = 100
-    rhoMax = 5000
-    radiusInit = 0.000001
+    rhoMax = 8200
+    log_active = False
+    rho = np.array([606.25, 2023.75, 403.75, 4150.0])
+    labels = ['GR (%.2f)' %rho[0], 'GR (%.2f)' %rho[1], 'ER (%.2f)' %rho[2], 'ER (%.2f)' %rho[3]]
+    rho = rho*cst.eV*10**6/(cst.c**2*cst.fermi**3)
+    dilaton_active = [False, False, True, True]
+    colors = ['blue', 'green', 'red', 'orange']
+    linestyles = ['-', '-', '--', '--']
+    for i in range(4):
+        tov = TOV(rho[i], PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active[i], log_active)
+        tov.ComputeTOV()
+        plt.plot(tov.radius/1000, tov.g_rr*tov.g_tt, color=colors[i], linestyle=linestyles[i], label = labels[i])
+        plt.axvline(tov.radiusStar/1000, color=colors[i], linestyle=linestyles[i])
+    plt.axis([5, 40, 0.2, 1.2])
+    plt.legend()
+    plt.show()
+
+def plotFig5():
+    PhiInit = 1
     PsiInit = 0
     option = 1
-    Phi = linspace(0.5,6,3)
-    rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in linspace(rhoMin,rhoMax,3)]
-    Phi_infini = zeros((len(Phi),len(rho)))
-    for i in range(len(Phi)):
-        print(Phi[i])
-        for j in range(len(rho)):
-            PhiInit = Phi[i]
-            rhoInit = rho[j]
-            tov = TOV(radiusInit, rhoInit, PsiInit, PhiInit, radiusStep, option)
-            tov.Compute()
-            Phi_infini[i][j] = tov.Phi_infini()
-    figure()
-    imshow(Phi_infini,extent=[min(rho)/(cst.eV*10**6/(cst.c**2*cst.fermi**3)), max(rho)/(cst.eV*10**6/(cst.c**2*cst.fermi**3)), min(Phi), max(Phi)], aspect='auto', origin='lower')
-    title("Dilaton $\Phi_\u221e$")
-    ylabel("Dilaton $\Phi_0$")
-    xlabel("Density $\\rho$ ($Mev/fm^3$)")
-    colorbar()
+    radiusMax_in = 50000
+    radiusMax_out = 10000000
+    Npoint = 50000
+    rhoMin = 100
+    rhoMax = 8200
+    log_active = False
+    rho = np.array([606.25, 2023.75, 403.75, 4150.0])
+    labels = ['GR (%.2f)' %rho[0], 'GR (%.2f)' %rho[1], 'ER (%.2f)' %rho[2], 'ER (%.2f)' %rho[3]]
+    rho = rho*cst.eV*10**6/(cst.c**2*cst.fermi**3)
+    dilaton_active = [False, False, True, True]
+    colors = ['blue', 'green', 'red', 'orange']
+    linestyles = ['-', '-', '--', '--']
+    for i in range(4):
+        tov = TOV(rho[i], PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active[i], log_active)
+        tov.ComputeTOV()
+        plt.plot(tov.radius/1000, np.sqrt(tov.g_rr/tov.g_tt), color=colors[i], linestyle=linestyles[i], label = labels[i])
+        plt.axvline(tov.radiusStar/1000, color=colors[i], linestyle=linestyles[i])
+    plt.axis([0, 100, 1.0, 2.75])
+    plt.legend()
+    plt.show()
 
-def plotPhi():
-    rhoInit = 1000*cst.eV*10**6/(cst.c**2*cst.fermi**3)
 
-
-    Phi0 = linspace(0.5,6,20)
-    M_Phi =  zeros((len(Phi0),1000))
-    for i in range(len(Phi0)):
-        print(Phi0[i])
-        PhiInit = Phi0[i]
-        tov = TOV(radiusInit, rhoInit, PsiInit, PhiInit, radiusStep, option)
-        tov.Compute()
-        M = [x for x in tov.Phi_r()]
-        for j in range(len(M)):
-            M_Phi[i][j] = M[j]-PhiInit
-    figure()
-    imshow(M_Phi,extent=[0, 1000, min(Phi0), max(Phi0)], aspect='auto', origin='lower')
-    title("Dilaton $\Phi$ - $\Phi_0$")
-    ylabel("Dilaton $\Phi_0$")
-    xlabel("Radius (km)")
-    colorbar()
-
-#test()
 def main():
-    test()
-    #plotRadiusMass(0, 1, 1, True)
-    #plt.show()
-    #plotMass()
-    #plotPhi_infini()
-    #plotPhi()
+    #test()
+    #plotRelation()
+    findSameMass(1.25)
+    #plotFig4()
+    #plotFig5()
 
 main()
