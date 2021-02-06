40297c2bc231b9b661576ed4dbc798b7df9880eb
denis <denis.arruga@gmail.com>
2021-02-06 16:13:17 +0100

    hot spot bolometric flux computation

---
+++ hot_spot.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+from TOV import *
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import scipy.constants as cst
+from scipy.integrate import trapz as integ
+from pynverse import inversefunc
+from scipy.interpolate import interp1d
+from scipy.misc import derivative as dd
+import numpy as np
+import math
+
+def Psi(sigma, a, b, r):
+    psi = np.zeros(len(sigma))
+    r_ = np.copy(r)
+    dr = 0.1*(r_[1]-r_[0])
+    R = r_[0]
+    r_[0] = R+dr
+    for i in range(len(sigma)):
+        v1 = np.sqrt(a[0]*b[0])/(R**2)*(-2*np.sqrt(1-sigma[i]**2*a[0]/r_[0]**2)/r_[0]**2)*dr
+        v2 = integ(np.sqrt(a*b)/(r_**2*np.sqrt(1-a*sigma[i]**2/r_**2)),r_)
+        psi[i] = sigma[i]*(v1+v2)
+    return psi
+
+def Shapiro(sigma, a, b, r):
+    shapiro = np.zeros(len(sigma))
+    r_ = np.copy(r)
+    dr = 0.1*(r_[1]-r_[0])
+    R = r_[0]
+    r_[0] = R+dr
+    for i in range(len(sigma)):
+        v1 = (np.sqrt(b[0]/a[0])*(-2*np.sqrt(1-sigma[i]**2*a[0]/r_[0]**2)/r_[0]**2)-np.sqrt(b[0]/a[0]))*dr
+        v2 = integ(np.sqrt(b/a)/(np.sqrt(1-a*sigma[i]**2/r_**2)),r_) -integ(np.sqrt(b/a),r_)
+        shapiro[i] = (v1+v2)/cst.c
+    return shapiro
+
+def ComputeAlphaAndDerivative(cPsi, sAlpha, cPsi_f_sAlpha):
+    sAlpha_ = np.zeros(len(cPsi))
+    DCPsi_DSAlpha = np.zeros(len(cPsi))
+    for i in range(len(cPsi)):
+        idx = len(np.where(cPsi_f_sAlpha<psi[i])[-1])
+        if idx != len(cPsi_f_sAlpha):
+            a = (sAlpha[idx+1]-sAlpha[idx])/(cPsi_f_sAlpha[idx+1]-cPsi_f_sAlpha[idx])
+            b = sAlpha[idx]-sAlpha[idx]*a
+            sAlpha_[i]= psi[i]*a+b
+            DCPsi_DSAlpha[i] = a
+    return sAlpha_, DCPsi_DSAlpha
+
+
+def ComputeFlux():
+    # initialize variable
+    PhiInit = 1
+    PsiInit = 0
+    option = 1
+    radiusMax_in = 40000
+    radiusMax_out = 100000000
+    Npoint = 600000
+    log_active = True
+    colorlist = {}
+    labels = {}
+    rho0 = 10000
+    rhoInit = rho0*cst.eV*10**6/(cst.c**2*cst.fermi**3)
+    iota = np.pi/4
+    gamma = np.pi/4
+    f = 400
+    omega = 2*np.pi*f
+    t0 = np.linspace(0,4*10**(-3),100)
+    for i in range(2):
+        if i==0:
+            dilaton_active = True
+            colorlist[0] = 'orangered'
+            labels[0] = 'ER'
+        else:
+            dilaton_active = False
+            colorlist[1] = 'k'
+            labels[1] = 'GR'
+        # Compute NS exterior space-time
+        tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active, log_active)
+        tov.ComputeTOV()
+        R = tov.radiusStar
+        r = tov.r_ext
+        a = tov.g_tt_ext
+        b = tov.g_rr_ext
+        phi_R = tov.phiStar
+        # Compute Psi(alpha) and plot psi-alpha and delta_t-alpha
+        alpha = np.linspace(0,np.pi/2,100)
+        sigma = R*np.sin(alpha)/np.sqrt(a[0])
+        f_psi_alpha = Psi(sigma, a, b, r)
+        plt.figure(1)
+        plt.plot(alpha,f_psi_alpha,color=colorlist[i])
+        plt.xlabel('$\\alpha$ [rad]')
+        plt.ylabel('$\\psi$ [rad]')
+        t = Shapiro(sigma, a, b, r)
+        plt.figure(2)
+        plt.plot(alpha,(t-t[0])*1000,color=colorlist[i])
+        plt.xlabel('$\\alpha$ [rad]')
+        plt.ylabel('t [ms]')
+        # Compute flux
+        psi = np.arccos(np.cos(iota)*np.cos(gamma)+np.sin(iota)*np.sin(gamma)*np.cos(omega*t0))
+        id_nvis = np.where(psi>f_psi_alpha[-1])
+        psi[id_nvis] = f_psi_alpha[-1]
+        f_1 = interp1d(f_psi_alpha, alpha, kind='cubic')
+        alpha_2 = f_1(psi)
+        df_1 = interp1d(f_psi_alpha, np.gradient(alpha)/np.gradient(f_psi_alpha), kind='cubic')
+        dAlpha_dPsi = df_1(psi)
+        beta = R*omega*np.sin(gamma)/np.sqrt(a[0])
+        cosXi = -np.sin(alpha_2)*np.sin(iota)*np.sin(omega*t0)/np.sin(psi)
+        delta = np.sqrt(1-(beta/cst.c)**2)/(1-(beta/cst.c)*cosXi)
+        Flux = phi_R*delta**5*a[0]*np.cos(alpha_2)*np.sin(alpha_2)*dAlpha_dPsi/np.sin(psi)
+        Flux[id_nvis] = 0
+        # Compute observed time
+        sigma = R*np.sin(alpha_2)/np.sqrt(a[0])
+        delta_t = Shapiro(sigma, a, b, r)
+        t_obs = t0+delta_t-delta_t[0]
+        plt.figure(3)
+        plt.plot(alpha,f_psi_alpha,color=colorlist[i])
+        plt.plot(alpha_2,psi,'o')
+        plt.xlabel('$\\alpha$')
+        plt.ylabel('$\\psi$')
+        plt.figure(4)
+        plt.plot(t_obs*1000,Flux,color=colorlist[i],label=labels[i])
+        plt.xlabel('observer time [ms]')
+        plt.ylabel('Noralized flux')
+        plt.legend()
+        plt.title('$\\iota=${:.2f}, $\gamma=${:.2f}, $\\rho_0=$ {:.1e} $Mev/fm^3$'.format(iota,gamma,rho0))
+    plt.show()
+
+ComputeFlux()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from TOV import *
+from TOV_2 import *
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy.constants as cst
@@ -10,16 +10,39 @@ def unit_test():
     PhiInit = 1
     PsiInit = 0
     option = 1
-    radiusMax_in = 500000
-    radiusMax_out = 1000000
-    Npoint = 400000
+    radiusMax_in = 40000
+    radiusMax_out = 100000
+    Npoint = 100000
     log_active = True
-    dilaton_active = True
+    dilaton_active = False
     rhoInit = 100*cst.eV*10**6/(cst.c**2*cst.fermi**3)
     tov = TOV(rhoInit, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, dilaton_active, log_active)
     tov.ComputeTOV()
+
     tov.Plot()
     tov.PlotMetric()
+    plt.axvline(23532/1000, color='r')
+    r = tov.radius
+    a = tov.g_tt
+    b = tov.g_rr
+    phi_dot = tov.Psi
+    a_dot = (-a[1:-2]+a[2:-1])/(r[2:-1]-r[1:-2])
+    b_dot = (-b[1:-2]+b[2:-1])/(r[2:-1]-r[1:-2])
+
+    plt.plot(r[1:-2]/1000,-a_dot*r[1:-2]*r[1:-2]/1000, label='$f_a$')
+    plt.plot(r[1:-2]/1000,-b_dot*r[1:-2]*r[1:-2]/1000, label='$f_b$')
+    plt.plot(r/1000,-phi_dot*r*r/1000, label='$f_\Phi$')
+    plt.legend()
+    plt.xlabel('radius [km]')
+    plt.ylabel('[km]')
+    plt.show()
+    plt.plot(a_dot, marker = 'o')
+    plt.show()
+
+    p = tov.pressure
+    plt.plot(r[1:-2],(-p[1:-2]+p[2:-1])/(r[2:-1]-r[1:-2]),'o')
+    plt.axvline(tov.radiusStar)
+    plt.show()
 
 def findSameMass(mass):
     """
@@ -136,6 +159,55 @@ def plotRelation():
     plt.legend()
     plt.show()
 
+def plotFig1_2():
+    PhiInit = 1
+    PsiInit = 0
+    option = 1
+    radiusMax_in = 50000
+    radiusMax_out = 10000000
+    Npoint = 50000
+    rhoMin = 100
+    rhoMax = 8200
+    log_active = False
+    massStar_GR = []
+    massStar_ER = []
+    radiusStar_GR = []
+    radiusStar_ER = []
+    rho = [x*cst.eV*10**6/(cst.c**2*cst.fermi**3) for x in range(rhoMin,rhoMax,5)]
+    for iRho in rho:
+        rhoInit = iRho
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, False, log_active)
+        tov.ComputeTOV()
+        massStar_GR.append(tov.massStar)
+        radiusStar_GR.append(tov.radiusStar)
+        tov = TOV(iRho, PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, True, log_active)
+        tov.ComputeTOV()
+        massStar_ER.append(tov.massStar)
+        radiusStar_ER.append(tov.radiusStar)
+    massStar_GR = [x/(1.989*10**30) for x in massStar_GR]
+    massStar_ER = [x/(1.989*10**30) for x in massStar_ER]
+    rho = [x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho]
+    plt.plot(rho, massStar_GR, label='General Relativity $M_{max}$ = '+str(round(max(massStar_GR),1))+': $\\rho_0$ ='+str(rho[massStar_GR.index(max(massStar_GR))]))
+    plt.plot(rho, massStar_ER, label='Entangled Relativity $M_{max}$ = '+str(round(max(massStar_ER),1))+': $\\rho_0$ ='+str(rho[massStar_ER.index(max(massStar_ER))]))
+    plt.xlabel('Density $\\rho$ ($Mev/fm^3$)')
+    plt.ylabel('Mass $M/M_{\odot}$')
+    plt.axvline(x=rho[massStar_GR.index(max(massStar_GR))])
+    plt.axvline(x=rho[massStar_ER.index(max(massStar_ER))])
+    plt.legend()
+    plt.show()
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/1000 for x in radiusStar_GR], label='GR')
+    plt.plot([x/(cst.eV*10**6/(cst.c**2*cst.fermi**3)) for x in rho], [x/1000 for x in radiusStar_ER], label='ER')
+    plt.xlabel('Density $\\rho$ ($Mev/fm^3$)')
+    plt.ylabel('Radius (km)')
+    plt.legend()
+    plt.show()
+    plt.plot([x/1000 for x in radiusStar_GR], [x/(1.989*10**30) for x in massStar_GR], label='GR')
+    plt.plot([x/1000 for x in radiusStar_ER], [x/(1.989*10**30) for x in massStar_ER], label='ER')
+    plt.xlabel('Radius (km)')
+    plt.ylabel('Mass $M/M_{\odot}$')
+    plt.legend()
+    plt.show()
+
 def plotFig4():
     PhiInit = 1
     PsiInit = 0
@@ -189,6 +261,7 @@ def plotFig5():
 
 def main():
     unit_test()
+    #plotFig1_2()
     #plotRelation()
     #findSameMass(1.25)
     #plotFig4()
